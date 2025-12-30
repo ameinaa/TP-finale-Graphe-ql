289725bc5cf7659b64a34b6636b971cf40742823
amina boukrouh <amienaboukrouh@gmail.com>
2025-12-30 16:21:23 +0100

    up date

--- src/main/java/rs/spai/demoQL/BookInput.java
+++ src/main/java/rs/spai/demoQL/BookInput.java
@@ -1,5 +1,15 @@
 package rs.spai.demoQL;
 
-public class BookInput {
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookInput {
+    private String title;
+    private Integer publicationYear;
+    private String language;
+    private Integer nbPages;
+    private Integer idC;       
+    private Integer idAuthor; 
 }
